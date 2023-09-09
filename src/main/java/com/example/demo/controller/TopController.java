3e5322309885b0cc61fef0cd7c769a305f9115ff
somen0101 <so_men0101@outlook.jp>
2023-09-10 00:46:56 +0900

    topのモデル作成およびモデルからの受け渡しテスト

--- src/main/java/com/example/demo/controller/TopController.java
+++ src/main/java/com/example/demo/controller/TopController.java
@@ -7,12 +7,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+import com.example.demo.viewmodel.TopViewModel;
+
 @Controller
 @EnableAutoConfiguration
 public class TopController {
+
+    TopViewModel topViewModel = new TopViewModel();
     @RequestMapping(value="/")
-    public String index(Model model){
-        model.addAttribute("message","Hello,word!!!");
-        return "index";
+    public String top(Model model){
+        model.addAttribute("teamName",topViewModel.getTeamName());
+        model.addAttribute("nowLeagueRank",topViewModel.getNowLeagueRank());
+        return "top";
     }
 }
---
+++ src/main/java/com/example/demo/viewmodel/TopViewModel.java
@@ -0,0 +1,16 @@
+package com.example.demo.viewmodel;
+
+public class TopViewModel {
+
+    private String teamName = "阪神タイガース";
+
+    private int nowLeagueRank = 1;
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public int getNowLeagueRank() {
+        return nowLeagueRank;
+    }
+}
